948533fb8bfa746f5d3e44083d62fc701f9067cc
aksardana12-dotcom <aksardana12@gmail.com>
2026-07-14 19:36:11 +0000

    my second project

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import random
+import math
 
-st.title("🎈 My new app")
+st.title("🎈 this is a ballon")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
